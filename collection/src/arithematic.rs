aa3f287a0ca9a8dbf81f28a6028b78f9709066ad
Arnav Sharma <arnav4324@gmail.com>
2026-05-02 21:30:57 +0530

    feat: added add_weighted()

--- collection/src/arithematic.rs
+++ collection/src/arithematic.rs
@@ -5,7 +5,7 @@ use numpy::{
 };
 
 #[pyfunction]
-fn add_images<'py>(py: Python<'py>, img1:PyReadonlyArrayDyn<u8>,img2:PyReadonlyArrayDyn<u8>)->PyResult<PyArrayDyn<u8>>{
+fn add_images<'py>(py: Python<'py>, img1:PyReadonlyArrayDyn<'py,u8>,img2:PyReadonlyArrayDyn<'py,u8>)->PyResult<PyArrayDyn<u8>>{
 
     let arr1 = img1.as_array();
     let arr2 = img2.as_array();
@@ -20,7 +20,7 @@ fn add_images<'py>(py: Python<'py>, img1:PyReadonlyArrayDyn<u8>,img2:PyReadonlyA
 }
 
 #[pyfunction]
-fn sub_images<'py>(py: Python<'py>, img1:PyReadonlyArrayDyn<u8>,img2:PyReadonlyArrayDyn<u8>)->PyResult<PyArrayDyn<u8>>{
+fn sub_images<'py>(py: Python<'py>, img1:PyReadonlyArrayDyn<'py,u8>,img2:PyReadonlyArrayDyn<'py,u8>)->PyResult<PyArrayDyn<u8>>{
 
     let arr1 = img1.as_array();
     let arr2 = img2.as_array();
@@ -32,4 +32,20 @@ fn sub_images<'py>(py: Python<'py>, img1:PyReadonlyArrayDyn<u8>,img2:PyReadonlyA
     Zip::from(&mut result).and(&arr1).and(&arr2).for_each(|(r,a,b)| {*r.a.saturating_sub(b);});
     
     Ok(result.into_pyarray_bound(py).into())   
+}
+
+#[pyfunction]
+fn add_weighted<'py>(py: Python<'py>, img1: PyReadonlyArrayDyn<'py, u8>, alpha: f64, img2: PyReadonlyArrayDyn<'py, u8>, beta: f64, gamma: f64) -> PyResult<Py<PyArrayDyn<u8>>> {
+    let arr1 = img1.as_array();
+    let arr2 = img2.as_array();
+    if arr1.shape() != arr2.shape() {
+        return Err(pyo3::exceptions::PyValueError::new_err("Images must have the exact same shape"));
+    }
+    
+    let mut result = numpy::ndarray::ArrayDyn::<u8>::zeros(arr1.shape());
+    Zip::from(&mut result).and(&arr1).and(&arr2).for_each(|res, &a, &b| {
+        let val = (a as f64 * alpha) + (b as f64 * beta) + gamma;
+        *res = val.clamp(0.0, 255.0) as u8;
+    });
+    Ok(result.into_pyarray_bound(py).into())
 }
