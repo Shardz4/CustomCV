b34f380e701a2cd6bd366bb99f23995f06dcd711
Arnav Sharma <arnav4324@gmail.com>
2026-05-02 21:24:07 +0530

    feat: Added sub_images

--- collection/src/arithematic.rs
+++ collection/src/arithematic.rs
@@ -16,7 +16,20 @@ fn add_images<'py>(py: Python<'py>, img1:PyReadonlyArrayDyn<u8>,img2:PyReadonlyA
     let mut result = numpy::ndarray::ArrayDyn::<u8>::zeros(arr1.shape());
     Zip::from(&mut result).and(&arr1).and(&arr2).for_each(|(r,a,b)| {*r.a.saturating_add(b);});
     
-    Ok(result.into_pyarray_bound(py).into())
+    Ok(result.into_pyarray_bound(py).into())   
+}
 
+#[pyfunction]
+fn sub_images<'py>(py: Python<'py>, img1:PyReadonlyArrayDyn<u8>,img2:PyReadonlyArrayDyn<u8>)->PyResult<PyArrayDyn<u8>>{
+
+    let arr1 = img1.as_array();
+    let arr2 = img2.as_array();
     
+    if arr1.shape() != arr2.shape() {
+        return Err(pyo3::exceptions::PyValueError::new_err("Images must have the same shape"))
+    }
+    let mut result = numpy::ndarray::ArrayDyn::<u8>::zeros(arr1.shape());
+    Zip::from(&mut result).and(&arr1).and(&arr2).for_each(|(r,a,b)| {*r.a.saturating_sub(b);});
+    
+    Ok(result.into_pyarray_bound(py).into())   
 }
