d90b872a854d696e131a15346ab3ee1755748089
Arnav Sharma <arnav4324@gmail.com>
2026-07-14 23:06:51 +0530

    feat: implement imwrite function (2/4)

--- collection/src/imgcodecs.rs
+++ collection/src/imgcodecs.rs
@@ -12,3 +12,21 @@ pub fn imread<'py>(
     let res = cv2.call_method1("imread", (filename, flags))?;
     Ok(res.into())
 }
+
+/// Writes an image to a specified file.
+#[pyfunction(name = "imwrite")]
+#[pyo3(signature = (filename, img, params = None))]
+pub fn imwrite<'py>(
+    py: Python<'py>,
+    filename: &str,
+    img: &pyo3::PyAny,
+    params: Option<Vec<i32>>,
+) -> PyResult<PyObject> {
+    let cv2 = py.import_bound("cv2")?;
+    let res = match params {
+        Some(p) => cv2.call_method1("imwrite", (filename, img, p))?,
+        None => cv2.call_method1("imwrite", (filename, img))?,
+    };
+    Ok(res.into())
+}
+
--- collection/src/lib.rs
+++ collection/src/lib.rs
@@ -260,5 +260,6 @@ fn rust_cv_lib(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
 
     // --- Image I/O ---
     m.add_function(wrap_pyfunction!(imgcodecs::imread, m)?)?;
+    m.add_function(wrap_pyfunction!(imgcodecs::imwrite, m)?)?;
     Ok(())
 }
