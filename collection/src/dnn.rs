a0f208941e2e43f8a31d00f0f9a3d7f7f17e955d
Arnav Sharma <arnav4324@gmail.com>
2026-07-18 20:00:49 +0530

    docs: document dnn.rs

--- collection/src/dnn.rs
+++ collection/src/dnn.rs
@@ -8,11 +8,25 @@ pub struct Net {
 
 #[pymethods]
 impl Net {
+    /// Net::new() - Constructor for Net wrapper.
+    /// @inner: Underlying OpenCV Net PyObject.
+    ///
+    /// Wraps an OpenCV dnn Net PyObject into the Net struct.
+    ///
+    /// Return: A Net instance.
     #[new]
     pub fn new(inner: PyObject) -> Self {
         Net { inner }
     }
 
+    /// Net::setInput() - Set input blob for the network.
+    /// @py: Python interpreter token.
+    /// @blob: Input blob.
+    /// @name: Name of the input layer.
+    ///
+    /// Sets the new value for the input of the specified layer name.
+    ///
+    /// Return: PyResult indicating success or failure.
     #[pyo3(signature = (blob, name = ""))]
     pub fn setInput<'py>(
         &self,
@@ -25,6 +39,13 @@ impl Net {
         Ok(())
     }
 
+    /// Net::forward() - Run forward pass of the network.
+    /// @py: Python interpreter token.
+    /// @output_name: Optional name of output layer to compute.
+    ///
+    /// Runs forward pass to compute output of the specified layer.
+    ///
+    /// Return: Network output PyObject.
     #[pyo3(signature = (output_name = None))]
     pub fn forward<'py>(
         &self,
@@ -40,7 +61,15 @@ impl Net {
     }
 }
 
-/// Loads a neural network from file.
+/// read_net() - Loads a neural network from file.
+/// @py: Python interpreter token.
+/// @model: Path to the model file.
+/// @config: Optional path to the configuration file.
+/// @framework: Optional name of the framework.
+///
+/// Loads a neural network using OpenCV's readNet function.
+///
+/// Return: A Net instance wrapper.
 #[pyfunction(name = "readNet")]
 #[pyo3(signature = (model, config = None, framework = None))]
 pub fn read_net<'py>(
@@ -59,7 +88,13 @@ pub fn read_net<'py>(
     Ok(Net { inner: res.into() })
 }
 
-/// Loads a neural network from ONNX format.
+/// read_net_from_onnx() - Loads a neural network from ONNX format.
+/// @py: Python interpreter token.
+/// @onnx_file: Path to the ONNX file.
+///
+/// Loads an ONNX format neural network.
+///
+/// Return: A Net instance wrapper.
 #[pyfunction(name = "readNetFromONNX")]
 pub fn read_net_from_onnx<'py>(
     py: Python<'py>,
@@ -71,7 +106,14 @@ pub fn read_net_from_onnx<'py>(
     Ok(Net { inner: res.into() })
 }
 
-/// Loads a neural network from Tensorflow format.
+/// read_net_from_tensorflow() - Loads a neural network from Tensorflow format.
+/// @py: Python interpreter token.
+/// @model: Path to the model file.
+/// @config: Optional path to the configuration file.
+///
+/// Loads a Tensorflow format neural network.
+///
+/// Return: A Net instance wrapper.
 #[pyfunction(name = "readNetFromTensorflow")]
 #[pyo3(signature = (model, config = None))]
 pub fn read_net_from_tensorflow<'py>(
@@ -88,7 +130,19 @@ pub fn read_net_from_tensorflow<'py>(
     Ok(Net { inner: res.into() })
 }
 
-/// Creates 4-dimensional blob from image.
+/// blob_from_image() - Creates 4-dimensional blob from image.
+/// @py: Python interpreter token.
+/// @image: Input image.
+/// @scalefactor: Multiplier for image values.
+/// @size: Spatial size of the output image.
+/// @mean: Mean subtraction values.
+/// @swap_rb: If true, swaps Red and Blue channels.
+/// @crop: If true, crops the image.
+/// @ddepth: Depth of the output blob.
+///
+/// Creates a 4-dimensional blob from an input image.
+///
+/// Return: The 4D blob PyObject.
 #[pyfunction(name = "blobFromImage")]
 #[pyo3(signature = (image, scalefactor = 1.0, size = (0, 0), mean = (0.0, 0.0, 0.0), swap_rb = false, crop = false, ddepth = 5))]
 pub fn blob_from_image<'py>(
@@ -115,7 +169,18 @@ pub fn blob_from_image<'py>(
     Ok(res.into())
 }
 
+/// nms_boxes() - Performs non-maximum suppression.
+/// @py: Python interpreter token.
+/// @bboxes: List of bounding boxes.
+/// @scores: List of confidence scores.
+/// @score_threshold: Score threshold.
+/// @nms_threshold: Non-maximum suppression threshold.
+/// @eta: Rate parameter.
+/// @top_k: Top K boxes to keep.
+///
 /// Performs non-maximum suppression given boxes and scores.
+///
+/// Return: Indices of boxes to keep.
 #[pyfunction(name = "NMSBoxes")]
 #[pyo3(signature = (bboxes, scores, score_threshold, nms_threshold, eta = 1.0, top_k = 0))]
 pub fn nms_boxes<'py>(
